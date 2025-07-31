1c59966073dc64c82371b94dc852c8cfdb565ae2
goinginblind <science.hhughes@gmail.com>
2025-07-31 11:08:43 +0200

    Chores: move main.go and add handlers.go - makes clearer separation of concerns

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strconv"
@@ -10,7 +11,25 @@ import (
 // Home page handler
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hello from the SnippetBox!\n"))
+
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
 }
 
 // Provides a page with a specific snippet
@@ -20,32 +39,17 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-
-	msg := fmt.Sprintf("Displaying a specific snippet with an id %d...\n", id)
+	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
 	w.Write([]byte(msg))
 }
 
 // Provides a page with a snippet creation page: a GET request must be sent
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Here a snippet would've been created\n"))
+	w.Write([]byte("Display a form for creating a new snippet..."))
 }
 
 // Creates a snippet: a POST request must be sent
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("Here a snippet would've been saved\n"))
-}
-
-// For now, the main entry point of the whole app
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-
-	log.Printf("Starting the server on :4000")
-
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	w.Write([]byte("Save a new snippet..."))
 }
---
+++ cmd/web/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	log.Print("starting server on :4000")
+	log.Fatalln(http.ListenAndServe(":4000", mux))
+}
