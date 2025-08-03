31c7f27418df179ca9d696aba11685be34ab1bbd
goinginblind <science.hhughes@gmail.com>
2025-08-03 23:02:05 +0200

    feat: add `snippetCreatePost` handler functionality - now correctly display form page - validate user input, restricting the title to not be empty or more than 100 chars long - content is not allowed to be empty - expiry date is allowed to only be 1, 7, 365 days - add an html page that is used to fill the 'create' form
    refactor: frontend nav template now has a button to access the 'create' form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/goinginblind/snippetbox/internal/models"
 )
@@ -49,16 +51,55 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
 // snippetCreate serves a page with a snippet creation page: a GET request must be sent
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a new snippet..."))
+	data := app.newTemplateData(r)
+	app.render(w, r, http.StatusOK, "create.tmpl", data)
 }
 
 // snippetCreatePost creates a snippet: a POST request must be sent.
 //
 // As of now it posts a placeholder to check the database functionality.
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n-Kabayashi Issa"
-	expires := 7
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// fieldErrors holds any validation errors for the form fields
+	fieldErrors := make(map[string]string)
+
+	// validate the 'title' field
+	if strings.TrimSpace(title) == "" {
+		fieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		fieldErrors["title"] = "This field cannot be more than 100 characters long"
+	}
+
+	// validate 'content' field
+	if strings.TrimSpace(content) == "" {
+		fieldErrors["content"] = "This field cannot be blank"
+	}
+
+	// validate expiry date
+	if expires != 1 && expires != 7 && expires != 365 {
+		fieldErrors["expires"] = "This field must be 1, 7 or 365"
+	}
+
+	// if there are any errors, dump them in plain HTTP text response
+	// and return from the handler
+	if len(fieldErrors) > 0 {
+		fmt.Fprint(w, fieldErrors)
+		return
+	}
 
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
