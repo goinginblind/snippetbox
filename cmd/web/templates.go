4d9717b80d486c358d94bcba13c27114cb907bf2
goinginblind <science.hhughes@gmail.com>
2025-08-03 18:59:36 +0200

    feat: the frontend now displays datetime in a more human readable format

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"github.com/goinginblind/snippetbox/internal/models"
 )
@@ -17,6 +18,20 @@ type templateData struct {
 	Snippets    []models.Snippet
 }
 
+// Create a human friendly representation of date time, which is a nicely
+// formatted string in a format such as "02 Jan 2006 at 15:04"
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap object and store it in a global variable. This is
+// essentially a string-keyed map which acts as a lookup between the names of our
+// custom template functions and the functions themselves.
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
+// Create a new cache with templates, uses a map to store them
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
@@ -28,7 +43,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
