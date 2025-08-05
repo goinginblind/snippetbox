dd8ab3b575ada229958c2dc54912f0aa9218e1b9
goinginblind <science.hhughes@gmail.com>
2025-08-05 18:26:32 +0200

    refactor: embed static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/goinginblind/snippetbox/ui"
 	"github.com/justinas/alice"
 )
 
@@ -10,8 +11,7 @@ import (
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
 	// Unprotected application routes, using the 'dynamic' middleware chain
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
---
+++ ui/efs.go
@@ -0,0 +1,6 @@
+package ui
+
+import "embed"
+
+//go:embed "static/css" "static/img" "static/js"
+var Files embed.FS
