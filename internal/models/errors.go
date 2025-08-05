5bba66899b362b39f2f2d4df083840f55b681c25
goinginblind <science.hhughes@gmail.com>
2025-08-05 11:16:13 +0200

    feat: add more custom errors for user signups. They provide further encapsulation and no reliance on underlying errors.

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -2,8 +2,18 @@ package models
 
 import "errors"
 
-// This error should be returned when no matching record has been found in the database.
-// It exists for full model encapsulation, so the handlers aren't concerned with the
-// underlying datastore or reliant on storage specific errors (like sql.ErrNoRows).
-// It's value is "models: no matching record found".
-var ErrNoRecord = errors.New("models: no matching record found")
+var (
+	// This error should be returned when no matching record has been found in the database.
+	// It exists for full model encapsulation, so the handlers aren't concerned with the
+	// underlying datastore or reliant on storage specific errors (like sql.ErrNoRows).
+	// It's value is "models: no matching record found".
+	ErrNoRecord = errors.New("models: no matching record found")
+
+	// Use this later if a user
+	// tries to login with an incorrect email address or password.
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+
+	// Use this later if a user
+	// tries to signup with an email address that's already in use.
+	ErrDuplicateEmail = errors.New("models: duplicate email")
+)
