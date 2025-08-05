83ac6ed7818b6b275d7c7a73dc559bd325104605
goinginblind <science.hhughes@gmail.com>
2025-08-05 15:45:28 +0200

    feat: add authentication requirement checking middlware for some of the routes

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -6,14 +6,6 @@ import (
 )
 
 // commonHeaders is a handler wrapper, whcih adds the common headers, mostly related to the privacy policy.
-//
-// How it is implemented:
-//
-//	w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
-//	w.Header().Set("X-Content-Type-Options", "nosniff")
-//	w.Header().Set("X-Frame-Options", "deny")
-//	w.Header().Set("X-XSS-Protection", "0")
-//	w.Header().Set("Server", "Go")
 func commonHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set(
@@ -57,6 +49,19 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 				app.serverError(w, r, fmt.Errorf("%s", err))
 			}
 		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) requireAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.IsAuthenticated(r) {
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+
+		w.Header().Add("Cache-Control", "no-store")
 
 		next.ServeHTTP(w, r)
 	})
